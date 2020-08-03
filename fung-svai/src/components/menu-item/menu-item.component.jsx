d6945d9e39233cc9b5ccc3fc29078decb8bd1b1f
Dboee <davidboee@gmail.com>
2020-08-03 21:18:04 +0200

    image bug bix

--- fung-svai/src/components/menu-item/menu-item.component.jsx
+++ fung-svai/src/components/menu-item/menu-item.component.jsx
@@ -3,7 +3,7 @@ import './menu-item.styles.scss';
 import { withRouter } from 'react-router-dom';
 
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-    <div className={` menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    <div className={`${size} menu-item `} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className='background-image' style={{ backgroundImage: `url(${imageUrl})` }} />
         <div className='content'>
             <h1 className='title'>{title.toUpperCase()}</h1>
