a36c536b900c939b00f3a74b640306d0b26887eb
Dboee <davidboee@gmail.com>
2020-09-14 08:38:02 +0200

    Setting up Redux 1

--- fung-svai/src/index.js
+++ fung-svai/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 
 import './index.css';
@@ -8,10 +9,12 @@ import App from './App';
 
 
 ReactDOM.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
