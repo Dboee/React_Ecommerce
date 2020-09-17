5c793eabcfce73baef0867d91037885ac6aef567
Dboee <davidboee@gmail.com>
2020-09-17 09:11:31 +0200

    Implementing Redux in cart

--- fung-svai/src/redux/cart/cart.reducer.js
+++ fung-svai/src/redux/cart/cart.reducer.js
@@ -1,4 +1,4 @@
-import cartActionTypes from './cart.types';
+import CartActionTypes from './cart.types';
 
 const INITIAL_STATE = {
     hidden: true
@@ -6,14 +6,14 @@ const INITIAL_STATE = {
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch (action.type) {
-        case cartActionTypes.TOGGLE_CART_HIDDEN:
+        case CartActionTypes.TOGGLE_CART_HIDDEN:
             return {
                 ...state,
                 hidden: !state.hidden
-            }
+            };
         default:
             return state;
     }
-}
+};
 
 export default cartReducer;
--- fung-svai/src/redux/cart/cart.types.js
+++ fung-svai/src/redux/cart/cart.types.js
@@ -1,3 +1,5 @@
-export default const CartActionTypes = {
+const CartActionTypes = {
     TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
 }
+
+export default CartActionTypes;
