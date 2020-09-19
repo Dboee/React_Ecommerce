062e1cf330c53b3eed64720f893399a137f3fdb9
Dboee <davidboee@gmail.com>
2020-09-19 13:21:18 +0200

    Reselect Library & Memoization

--- fung-svai/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ fung-svai/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -4,6 +4,8 @@ import {connect} from 'react-redux';
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
+import{selectCartItems} from '../../redux/cart/cart.selectors.js';
+
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({cartItems}) => (
@@ -20,8 +22,8 @@ const CartDropdown = ({cartItems}) => (
 );
 
 
-const mapStateToProps = ({cart: {cartItems}}) => ({
-    cartItems
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps) (CartDropdown);
--- fung-svai/src/components/cart-icon/cart-icon.component.jsx
+++ fung-svai/src/components/cart-icon/cart-icon.component.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
+
 import { toggleCartHidden } from '../../redux/cart/cart.actions';
+
+import {selectCartItemsCount} from '../../redux/cart/cart.selectors.js';
 
 import './cart-icon.styles.scss';
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
     <div className="cart-icon" onClick={toggleCartHidden}>
         <ShoppingIcon className="shopping-icon" />
-        <span className="item-count"> 0 </span>
+        <span className="item-count"> {itemCount} </span>
     </div>
 )
 
@@ -18,4 +21,8 @@ const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = (state) => ({
+  itemCount: selectCartItemsCount(state),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
