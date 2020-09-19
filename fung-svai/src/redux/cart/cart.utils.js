eb5f3a3b02da5294b318c61440708f5e0e2704a7
Dboee <davidboee@gmail.com>
2020-09-19 09:43:12 +0200

    Adding quantity to cart items

--- fung-svai/src/redux/cart/cart.utils.js
+++ fung-svai/src/redux/cart/cart.utils.js
@@ -8,7 +8,10 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     if (existingCartItem) {
         return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id
-                ? {}
+                ? {...cartItem, quantity: cartItem.quantity + 1}
+                : cartItem
         )
     }
-}
+    return [...cartItems, {...cartItemToAdd, quantity:1}]
+
+};
