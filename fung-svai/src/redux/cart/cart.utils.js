833187510a4f30fd434ffced65a3ee72f847e92a
Dboee <davidboee@gmail.com>
2020-09-20 18:57:22 +0200

    Remove items at Checkout

--- fung-svai/src/redux/cart/cart.utils.js
+++ fung-svai/src/redux/cart/cart.utils.js
@@ -8,10 +8,29 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     if (existingCartItem) {
         return cartItems.map(cartItem =>
             cartItem.id === cartItemToAdd.id
-                ? {...cartItem, quantity: cartItem.quantity + 1}
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
                 : cartItem
         )
     }
-    return [...cartItems, {...cartItemToAdd, quantity:1}]
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }]
 
+};
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const exisitingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if (exisitingCartItem.quantity === 1) {
+        return cartItems.filter(cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+        cartItem =>
+            cartItem.id === cartItemToRemove.id
+                ?
+                { ...cartItem, quantity: cartItem.quantity - 1 }
+                :
+                cartItem
+    )
 };
