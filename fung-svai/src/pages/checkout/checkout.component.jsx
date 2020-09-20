35e2f0c3922af3db28b94882cb20f8dcd451506e
Dboee <davidboee@gmail.com>
2020-09-20 17:04:04 +0200

    Checkout item component

--- fung-svai/src/pages/checkout/checkout.component.jsx
+++ fung-svai/src/pages/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import './checkout.styles.scss';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import { selectCartItems, selectCartTotal } from '../../redux/cart/cart.selectors';
 
 const CheckoutPage = ({ cartItems, total }) => (
@@ -28,7 +30,7 @@ const CheckoutPage = ({ cartItems, total }) => (
         </div>
         {
             cartItems.map(cartItem =>
-                cartItem.name
+                <CheckoutItem cartItem={cartItem} />
             )
         }
         <div className='total'>
