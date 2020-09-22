e8bd1e4cd8ad37cba5af5d3da5569c3f0a5097d6
Dboee <davidboee@gmail.com>
2020-09-22 17:11:54 +0200

    Collection State Into Redux

--- fung-svai/src/redux/directory/directory.selectors.js
+++ fung-svai/src/redux/directory/directory.selectors.js
@@ -1,5 +1,5 @@
 import {createSelector} from "reselect";
-import directoryReducer from "./directory.reducer";
+//import directoryReducer from "./directory.reducer";
 
 const selectDirectory = state => state.directory;
 
