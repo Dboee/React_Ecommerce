6ac89e3ea18d9ffff672c0a4223c51da4491449e
Dboee <davidboee@gmail.com>
2020-09-22 17:48:32 +0200

    Collections Overview Component

--- fung-svai/src/pages/shop/shop.component.jsx
+++ fung-svai/src/pages/shop/shop.component.jsx
@@ -1,24 +1,14 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {createStructuredSelector} from 'reselect';
 
-import {selectCollections} from '../../redux/shop/shop.selectors.js';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 const ShopPage =({collections}) => (
 
             <div className='shop-page'>
-                {
-                    collections.map(({ id, ...otherCollectionProps }) => (
-                        <CollectionPreview key={id} {...otherCollectionProps} />
-                    ))}
+              <CollectionsOverview/>
             </div>
         );
 
-const mapStateToProps = createStructuredSelector({
-    collections: selectCollections
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
