ed3935f33643eb9fe00cf3f5ee2869b5deb8990a
Dboee <davidboee@gmail.com>
2020-09-22 18:13:46 +0200

    Nested Routing in Shop Page

--- fung-svai/src/pages/shop/shop.component.jsx
+++ fung-svai/src/pages/shop/shop.component.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
+import CategoryPage from '../category/category.component';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
 
 
-const ShopPage =({collections}) => (
+const ShopPage =({match}) => (
 
             <div className='shop-page'>
-              <CollectionsOverview/>
+              <Route excact path={`${match.path}`} component={CollectionsOverview}/>
+              <Route path={`${match.path}/:categoryId`} component={CategoryPage} />
             </div>
         );
 
