698035627399f539c1757beba26f969eafaf224e
Dboee <davidboee@gmail.com>
2020-10-07 17:31:08 +0200

    typo fix

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true });
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
 app.use(cors());
 
